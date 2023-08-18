d3d1df479dda2ac0b88d8fb5f9e7a09aa153827c
yppelu <yppelu@gmail.com>
2023-08-18 19:45:25 +0700

    Fix a function

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ calculator.style.width = calculateMainBlockWidth();
 function calculateMainBlockWidth() {
   return (window.innerWidth > window.innerHeight)
     ? `${window.innerHeight * 0.5}px`
-    : width = `${window.innerWidth * 0.8}px`;
+    : `${window.innerWidth * 0.8}px`;
 }
