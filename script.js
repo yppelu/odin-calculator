33f6c3e9801568fc5675e4f6aaaa129aea0bbae3
yppelu <yppelu@gmail.com>
2023-08-19 19:57:20 +0700

    Add main variables

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const calculator = document.querySelector('.calculator');
 calculator.style.width = calculateMainBlockWidth();
 const display = document.querySelector('.display');
 
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
 function calculateMainBlockWidth() {
   return (window.innerWidth > window.innerHeight)
     ? `${window.innerHeight * 0.5}px`
