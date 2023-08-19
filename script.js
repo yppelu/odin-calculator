8d066e472d1471a5549e1afd311ff4a246dd72dc
yppelu <yppelu@gmail.com>
2023-08-19 20:05:24 +0700

    Add removeOneDigit function

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ function calculateMainBlockWidth() {
     ? `${window.innerHeight * 0.5}px`
     : `${window.innerWidth * 0.8}px`;
 }
+
+function removeOneDigit() {
+  if (operator === '') {
+    firstNumber = firstNumber.slice(0, -1);
+  } else {
+    secondNumber = secondNumber.slice(0, -1);
+  }
+}
